6f646a3dd81b424a6a394b2dca1b951bf2331cdb
Pedro Grilo <56405847+grilo13@users.noreply.github.com>
2021-06-08 00:47:06 +0100

    Delete Exericios 1st Try directory

--- Exericios 1st Try/Listas/list.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-typedef struct list list_t;
-
-list_t *list_new();
-bool list_insert(list_t *list, int value);
-void list_print(list_t *list);
-void list_destroy(list_t *list);
